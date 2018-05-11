59285a0634ec3600bf4eb2b2f97b183c50ffee08
kongdd.sysu <kongdd.sysu@gmail.com>
2018-05-11 01:44:44 +0000

    Updated usage/clip_regions.js (added 14 lines, deleted 5 lines).

--- usage/clip_regions.js
+++ usage/clip_regions.js
@@ -1,15 +1,24 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var pmlv2 = ee.ImageCollection("projects/pml_evapotranspiration/PML/OUTPUT/TEMP/PML_V2_yearly"),
-    pmlv1 = ee.ImageCollection("projects/pml_evapotranspiration/PML/OUTPUT/TEMP/PML_V1_yearly");
+var region = ee.FeatureCollection("projects/pml_evapotranspiration/Australia/COO_Lat_Lon"),
+    pml_v2 = ee.ImageCollection("projects/pml_evapotranspiration/PML/OUTPUT/PML_V2_8day");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var pkg_export = require('users/kongdd/public:pkg_export.js');
 
-var imgcol = pmlv2;
-imgcol = ee.ImageCollection(imgcol.toList(20));
+var imgcol = pml_v2;
+imgcol     = ee.ImageCollection(imgcol.toList(1000));
+
+imgcol = imgcol.map(function(img){
+    return img.clip(region);
+});
+
+print(region);
+print(imgcol.limit(10));
+
+Map.addLayer(region);
 
 var range  = [-180, -60, 180, 90],
     scale  = 1 / 120, //1/240,
     drive  = true,
     folder = 'PMLV2yearly'; //
 
-pkg_export.ExportImgCol(pmlv2, undefined, range, scale, drive, folder);
+// pkg_export.ExportImgCol(pmlv2, undefined, range, scale, drive, folder);
