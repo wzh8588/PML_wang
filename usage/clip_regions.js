aceb54642ab1282fbf43079615fcd7286ffc2b2e
kongdd.sysu <kongdd.sysu@gmail.com>
2018-05-11 02:16:46 +0000

    Updated usage/clip_regions.js (added 30 lines, deleted 13 lines).

--- usage/clip_regions.js
+++ usage/clip_regions.js
@@ -4,21 +4,38 @@ var region = ee.FeatureCollection("projects/pml_evapotranspiration/Australia/COO
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var pkg_export = require('users/kongdd/public:pkg_export.js');
 
+/** load imgcol*/
 var imgcol = pml_v2;
-imgcol     = ee.ImageCollection(imgcol.toList(1000));
+imgcol     = ee.ImageCollection(imgcol.toList(1000)).select([0, 1, 2, 3, 4]);
+// print(imgcol.limit(3));
 
-imgcol = imgcol.map(function(img){
-    return img.clip(region);
-});
+/** get bbox of assigned region */
+function getRange(region){
+    var bound = ee.Feature(region.first()).bounds();
+    bound = bound.geometry().coordinates().get(0).getInfo();
+    bound = ee.List(bound);
+    
+    var x = ee.Array(bound.get(0)).floor(),
+        y = ee.Array(bound.get(2)).ceil();
+        
+    var range = ee.Array.cat([x, y], 0).getInfo();
+    return range;
+}
+var range = getRange(region);
+print(range);
+// print(ee.Array(bound).accum(0, 'max'))
 
-print(region);
-print(imgcol.limit(10));
+// Map.addLayer(bound, {}, 'bounds');
+/** clip regional data */
+imgcol = imgcol.map(function(img){ return img.clip(region); });
 
-Map.addLayer(region);
+// print(region);
+// print(imgcol.limit(10));
+// Map.addLayer(region);
 
-var range  = [-180, -60, 180, 90],
-    scale  = 1 / 120, //1/240,
+// var range  = [-180, -60, 180, 90];
+var scale  = 1 / 20, //1/240,
     drive  = true,
-    folder = 'PMLV2yearly'; //
+    folder = 'PMLV2basin'; //
 
-// pkg_export.ExportImgCol(pmlv2, undefined, range, scale, drive, folder);
+pkg_export.ExportImgCol(imgcol, undefined, range, scale, drive, folder);
