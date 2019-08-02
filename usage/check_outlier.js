60fc62fa7814333bcbfef303d0253f61b969dfbf
Dongdong Kong <kongdd@users.noreply.github.com>
2019-08-02 19:41:23 +0800

    version 0.1.5 released

--- usage/check_outlier.js
+++ usage/check_outlier.js
@@ -1,10 +1,10 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var imgcol_pmlv2 = ee.ImageCollection("projects/pml_evapotranspiration/PML/OUTPUT/PML_V2_8day"),
-    imgcol_lai = ee.ImageCollection("MODIS/006/MCD15A3H"),
+var imgcol_lai = ee.ImageCollection("MODIS/006/MCD15A3H"),
     imgcol_evi = ee.ImageCollection("MODIS/006/MOD13A1"),
     img_gde = ee.Image("projects/pml_evapotranspiration/Cooper/COO_GDEs"),
     poly = ee.FeatureCollection("projects/pml_evapotranspiration/Cooper/COO_gde_poly"),
-    poly0 = ee.FeatureCollection("projects/pml_evapotranspiration/Cooper/COO_Lat_Lon");
+    poly0 = ee.FeatureCollection("projects/pml_evapotranspiration/Cooper/COO_Lat_Lon"),
+    imgcol_pmlv2 = ee.ImageCollection("projects/pml_evapotranspiration/PML/OUTPUT/PML_V2_8day_v014");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 /**
  * PML_V2 usage illustration 
@@ -14,30 +14,36 @@ var imgcol_pmlv2 = ee.ImageCollection("projects/pml_evapotranspiration/PML/OUTPU
 var pkg_vis    = require('users/kongdd/public:pkg_vis.js');
 var pkg_export = require('users/kongdd/public:pkg_export.js');
 
-
-imgcol_pmlv2 = imgcol_pmlv2.select([0, 1, 2, 3])
-    .map(function(img){
-        return img.clip(poly0).divide(100)
-            .copyProperties(img, ['system:time_start']);
-    });
+imgcol_pmlv2 = imgcol_pmlv2.limit(100);
+imgcol_pmlv2 = imgcol_pmlv2.map(function(img){
+    var ET = img.expression('b("Ei") + b("Ec") + b("Es")').rename('ET');
+    return img.addBands(ET);
+});
+// imgcol_pmlv2 = imgcol_pmlv2.select([0, 1, 2, 3])
+//     .map(function(img){
+//         return img.clip(poly0).divide(100)
+//             .copyProperties(img, ['system:time_start']);
+//     });
 
 /** 1. visualization */ 
 var palette = ['#570088', '#920057', '#CE0027', '#FF0A00', '#FF4500', '#FF8000', '#FFB100', '#FFD200', '#FFF200', '#C7EE03', '#70D209', '#18B80E', '#067F54', '#033FA9', '#0000FF'];
 var vis_gpp = { min: 0.0, max: 60.0, palette: palette.reverse(), bands: 'GPP'};
 var vis_et  = { min: 0.0, max: 1600.0, palette: palette.reverse(), bands: 'ET'};
+var vis_et  = { min: 0.0, max: 5.0, palette: palette.reverse(), bands: 'ET'};
 var vis_wue = { min: 0.0, max: 4.0   , palette: palette, bands: 'WUE'};
 
 var vis = true;
 if (vis){
     // Map.addLayer(PML_V2_yearly, vis_et , 'ET');
-    Map.addLayer(imgcol_pmlv2, {}, 'PMLV2 8days')
+    // Map.addLayer(imgcol_pmlv2.select('ET'), vis_et, 'PMLV2 8days')
+    
     // Map.addLayer(PML_V2_yearly, vis_gpp, 'GPP');
     // Map.addLayer(PML_V2_yearly, vis_et , 'ET');
     // Map.addLayer(PML_V2_yearly, vis_wue, 'WUE');
     
     // add a legend to Map
-    // pkg_vis.grad_legend(vis_et , '(a) annual average ET');
-    pkg_vis.grad_legend(vis_gpp, '(b) annual average GPP');
+    pkg_vis.grad_legend(vis_et , '(a) ET (mm d-1)');
+    // pkg_vis.grad_legend(vis_gpp, '(b) annual average GPP');
     
     // the left corner chart is imgcol annual in the region you defined
     // var label = ui.Label('2003-01-01'); Map.add(label);
@@ -56,7 +62,7 @@ Map.add(label);
 // var region = img_gde.geometry();
 // Map.addLayer(region, {}, 'aggregate region');
 
-series(imgcol_pmlv2, {}, 'imgcol_pmlv2 8-day', poly0, label);
+series(imgcol_pmlv2.select('ET'), vis_et, 'imgcol_pmlv2 8-day', poly0, label, 5000);
 // Map.centerObject(poly, 8);
 // Map.addLayer(poly, {}, 'BASIN');
 // Map.addLayer(img_gde, {}, 'gde');
